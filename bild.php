653c53bc6fe9e8b2079be63c26e25c81c283d571
JonGae007 <jongae007@gmail.com>
2023-11-20 16:35:32 +0100

    Update bild.php

--- bild.php
+++ bild.php
@@ -3,12 +3,9 @@ $im = imagecreatefromjpeg("bg.jpg");
 $textfarbe = imagecolorallocate($im, 255, 255, 255);
 $font_size = 7;
 $font_path = 'sa.ttf';
-imagettftext($im, $font_size, 0, 190, 40, $textfarbe, $font_path, 'Hello world!');
-imagettftext($im, $font_size, 0, 170, 40, $textfarbe, $font_path, 'Hello world!');
-// Gib das Bild aus
+imagettftext($im, $font_size, 0,190, 40, $textfarbe, $font_path, 'Hello world!');
+imagettftext($im, $font_size, 0,190, 50, $textfarbe, $font_path, 'Hello world!');
 header('Content-type: image/png');
 imagepng($im);
-
-// Zerstöre das Bild, um den Speicher freizugeben
 imagedestroy($im);
 ?>
